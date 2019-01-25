81db79a5b74f5214f81e6af317a16370b6bc23dd
Rolf <rolf.v.d.forst@fau.de>
2019-01-25 16:45:32 +0100

    Fix hidden-text shortcode

--- RRZE/Elements/HiddenText/HiddenText.php
+++ RRZE/Elements/HiddenText/HiddenText.php
@@ -4,13 +4,15 @@ namespace RRZE\Elements\HiddenText;
 
 defined('ABSPATH') || exit;
 
-class HiddenText {
-
-    public function __construct() {
+class HiddenText
+{
+    public function __construct()
+    {
         add_shortcode('hidden-text', [$this, 'shortcode_hidden_text']);
     }
 
-    public function shortcode_hidden_text($atts, $content = '') {
+    public function shortcode_hidden_text($atts, $content = '')
+    {
         extract(shortcode_atts([
             'start' => '',
             'end' => ''
@@ -18,19 +20,18 @@ class HiddenText {
 
         $now = current_time('timestamp');
 
-        $start = $start ? strtotime($start, $now) : $now;
-        $end = $end ? strtotime($end, $now) : $now;
+        $t_start = $start != '' ? strtotime($start, $now) : $now;
+        $t_end = $end != '' ? strtotime($end, $now) : $now;
 
-        if (! $start || ! $end) {
+        if ($t_start === false || $t_end === false) {
             return do_shortcode('[notice-attention]' . __('Please use a valid date format: Y-m-d H:i:s.', 'rrze-elements') . '[/notice-attention]' . $content);
         }
 
-        if ($now > $start || $now < $end) {
+        if (($start != '' && $now <= $t_start) || ($end != '' && $now >= $t_end)) {
             $output = '<p>' . do_shortcode($content) . '</p>';
         } else {
             $output = '';
         }
         return $output;
     }
-
 }
