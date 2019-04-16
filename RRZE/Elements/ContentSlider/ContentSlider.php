4776a1c0fb43f75f079f5742b4998c9d198d634f
Barbara Bothe <barbara.bothe@fau.de>
2019-04-16 11:11:19 +0200

    Excerpt in Content-Slider auch bei FAU-Themes
    https://github.com/RRZE-Webteam/rrze-elements/issues/51

--- RRZE/Elements/ContentSlider/ContentSlider.php
+++ RRZE/Elements/ContentSlider/ContentSlider.php
@@ -111,7 +111,7 @@ class ContentSlider {
                 $output .= '<li>';
                 $output .= '<h2>' . $link_open . get_the_title() . $link_close . '</h2>';
                 $output .= $link_open . get_the_post_thumbnail($id, 'medium_large', array('class' => 'attachment-teaser-thumb', 'style' => $img_style)) . $link_close;
-                $output .= get_the_excerpt($id);
+	            $output .= function_exists('fau_custom_excerpt') ? fau_custom_excerpt($id): get_the_excerpt($id);
                 $output .= '</li>';
             }
 
