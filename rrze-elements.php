c162a5147c5e9346a7db2352e6290d602df8f0f3
Barbara Bothe <barbara.bothe@fau.de>
2019-05-06 16:46:28 +0200

    Versionsnummer

--- rrze-elements.php
+++ rrze-elements.php
@@ -4,7 +4,7 @@
  * Plugin Name:     RRZE Elements
  * Plugin URI:      https://github.com/RRZE-Webteam/rrze-elements
  * Description:     Erweiterte Gestaltungselemente für WordPress-Websites
- * Version:         1.7.4
+ * Version:         1.7.5
  * Author:          RRZE-Webteam
  * Author URI:      https://blogs.fau.de/webworking/
  * License:         GNU General Public License v2
