0082b6c649a118bf52a814286eb0ff9f33e242af
Cassandre <info@barbara-bothe.de>
2020-03-25 20:23:48 +0100

    Content-Slider Format Carousel
    https://github.com/RRZE-Webteam/rrze-elements/issues/85

--- includes/ContentSlider/assets/js/rrze-flexslider.js
+++ includes/ContentSlider/assets/js/rrze-flexslider.js
@@ -5,7 +5,16 @@
  */
 
 jQuery(document).ready(function($) {
-    $('.flexslider').flexslider({
+    //$('a:not(.prev, .next)')
+    $('.flexslider:not(carousel)').flexslider({
+        pausePlay: true
+    });
+
+    $('.flexslider.carousel').flexslider({
+        animation: "slide",
+        animationLoop: true,
+        itemWidth: 300,
+        itemMargin: 5,
         pausePlay: true
     });
 });
