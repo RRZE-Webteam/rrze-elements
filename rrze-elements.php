bb8896fff1b95320b768995b2c7b586b68ae5993
Cassandre <info@barbara-bothe.de>
2021-02-15 11:22:57 +0100

    Bugfix columns

--- rrze-elements.php
+++ rrze-elements.php
@@ -4,7 +4,7 @@
 Plugin Name:     RRZE Elements
 Plugin URI:      https://github.com/RRZE-Webteam/rrze-elements
 Description:     Advanced design elements for WordPress websites.
-Version:         1.16.1
+Version:         1.16.2
 Author:          RRZE Webteam
 Author URI:      https://blogs.fau.de/webworking/
 License:         GNU General Public License v2
