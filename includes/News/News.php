9b68f6da25e5ac62ba5b00b0cb773220af6154b3
Cassandre <barbara.bothe@fau.de>
2022-01-20 18:16:23 +0100

    Bugfix RRZE / Anpassung neue FAU-Farben

--- includes/News/News.php
+++ includes/News/News.php
@@ -241,9 +241,9 @@ class News
         $moreLink = '';
         if (in_array('show_more', $mode)) {
             if ($cat != '') {
-                $moreLink = '<p class="more-posts"><a class="standard-btn xsmall-btn primary-btn" href="'.get_category_link($c_id[0]).'">' . __('Weitere Artikel', 'rrze-elements') . '</a></p>';
+                $moreLink = '<p class="more-posts"><a class="standard-btn xsmall-btn primary-btn" href="'.get_category_link($c_id[0]).'">' . __('More news', 'rrze-elements') . '</a></p>';
             } elseif ($tag != '') {
-                $moreLink = '<p class="more-posts"><a class="standard-btn xsmall-btn primary-btn" href="'.get_tag_link($t_id[0]).'">' . __('Weitere Artikel', 'rrze-elements') . '</a></p>';
+                $moreLink = '<p class="more-posts"><a class="standard-btn xsmall-btn primary-btn" href="'.get_tag_link($t_id[0]).'">' . __('More news', 'rrze-elements') . '</a></p>';
             }
         }
 
