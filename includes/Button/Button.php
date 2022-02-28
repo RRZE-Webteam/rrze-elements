413df84a6d024024fb49af27246a1f6fff490ca1
Cassandre <barbara.bothe@fau.de>
2022-02-28 19:53:55 +0100

    Update Button.php
    #173

--- includes/Button/Button.php
+++ includes/Button/Button.php
@@ -25,39 +25,45 @@ class Button
      */
     public function shortcodeButton($atts, $content = '')
     {
-        extract(shortcode_atts([
-            'link' => '#',
-            'target' => '',
-            'color' => '',
-            'border_color' => '',
-            'size' => '',
-            'width' => '',
-            'style' => '',
-            'font' => '',
-            'title' => '',
-        ], $atts));
+        $atts = shortcode_atts(
+            array(
+                'link' => '#',
+                'target' => '',
+                'color' => '',
+                'border_color' => '',
+                'size' => '',
+                'width' => '',
+                'style' => '',
+                'font' => '',
+                'title' => '',
+                'aria-label' => '',
+            ), $atts );
 
-        $style = (in_array($style, ['success', 'info', 'warning', 'danger', 'primary', 'ghost'])) ? ' ' . $style . '-btn' : ' primary-btn';
+        $arialabel = $atts['aria-label'] != '' ? ' aria-label="' . sanitize_text_field($atts['aria-label']) . '"' : '';
+        /*if ($arialabel != '' && stripos($arialabel, $content) === false) {
+            return do_shortcode('[alert style="danger"]' . __('<strong>Button shortcode error</strong>: The button text ("' . $content .  '") has to be included in the aria label ("' . sanitize_text_field($atts['aria-label']) .  '"). ' , 'rrze-elements') . '[/alert]');
+        }*/
+        $style = (in_array($atts['style'], ['success', 'info', 'warning', 'danger', 'primary', 'ghost'])) ? ' ' . $atts['style'] . '-btn' : ' primary-btn';
         $color_hex = '';
         $color_name = '';
 
-        if ((substr($color, 0, 1) == '#') && (in_array(strlen($color), [4, 7]))) {
+        if ((substr($atts['color'], 0, 1) == '#') && (in_array(strlen($atts['color']), [4, 7]))) {
             $color_name = '';
-            $color_hex = 'background-color:' . $color . ';';
+            $color_hex = 'background-color:' . $atts['color'] . ';';
             $style = 'X';
         }
 
-        if (in_array($color, ['red', 'yellow', 'blue', 'green', 'grey', 'black'])) {
-            $color_name = ' ' . $color . '-btn';
+        if (in_array($atts['color'], ['red', 'yellow', 'blue', 'green', 'grey', 'black'])) {
+            $color_name = ' ' . $atts['color'] . '-btn';
             $style = 'Y';
         }
 
-        $border_color = ((substr($border_color, 0, 1) == '#') && (in_array(strlen($border_color), [4, 7]))) ? ' border:1px solid' . $border_color . ';' : '';
+        $border_color = ((substr($atts['border_color'], 0, 1) == '#') && (in_array(strlen($atts['border_color']), [4, 7]))) ? ' border:1px solid' . $atts['border_color'] . ';' : '';
 
-        $size = ($size) ? ' ' . $size . '-btn' : '';
-        $target = ($target == 'blank') ? ' target="_blank"' : '';
-        $link = esc_url($link);
-        switch ($font) {
+        $size = ($atts['size']) ? ' ' . $atts['size'] . '-btn' : '';
+        $target = ($atts['target'] == 'blank') ? ' target="_blank"' : '';
+        $link = esc_url($atts['link']);
+        switch ($atts['font']) {
             case 'dark':
                 $fontColor = ' color: #1a1a1a;';
                 break;
@@ -67,8 +73,8 @@ class Button
             default:
                 $fontColor = '';
         }
-        $title = $title != '' ? ' title="' . sanitize_text_field($title) . '"' : '';
-        $width = trim($width);
+        $title = $atts['title'] != '' ? ' title="' . sanitize_text_field($atts['title']) . '"' : '';
+        $width = trim($atts['width']);
         $width_full = '';
         $width_px = '';
         if ($width == 'full') {
@@ -79,11 +85,11 @@ class Button
             $width = $width . 'px';
         }
 
-        if ('' != $color || '' != $fontColor) {
+        if ('' != $atts['color'] || '' != $fontColor) {
             $style = '';
         }
 
-        $output = '<a' . $target . $title . ' class="standard-btn' . $color_name . $size . $width_full . $style . '" href="' . $link . '" style="' . $fontColor . $color_hex . $width_px . $border_color . '"><span>' . do_shortcode($content) . '</span></a>';
+        $output = '<a' . $target . $title . $arialabel . ' class="standard-btn' . $color_name . $size . $width_full . $style . '" href="' . $link . '" style="' . $fontColor . $color_hex . $width_px . $border_color . '"><span>' . do_shortcode($content) . '</span></a>';
 
         wp_enqueue_style('fontawesome');
         wp_enqueue_style('rrze-elements');
