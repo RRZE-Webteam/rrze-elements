f3ae9b3e2eac7131cf57dcb1ab3e881ca68dae6e
Barbara Bothe <barbara.bothe@fau.de>
2022-05-31 16:41:09 +0200

    CSS-Fix Kontakt in Flexslider
    fixes #165

--- rrze-elements.php
+++ rrze-elements.php
@@ -4,7 +4,7 @@
 Plugin Name:     RRZE Elements
 Plugin URI:      https://github.com/RRZE-Webteam/rrze-elements
 Description:     Advanced design elements for WordPress websites.
-Version:         1.22.10
+Version:         1.22.11
 Author:          RRZE Webteam
 Author URI:      https://blogs.fau.de/webworking/
 License:         GNU General Public License v2
