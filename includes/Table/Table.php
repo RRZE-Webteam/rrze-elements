18f9e16635b367b8add82d1b43b348104092a5fb
Barbara Bothe <barbara.bothe@fau.de>
2023-03-15 08:59:44 +0100

    Update Table.php
    fixes #229

--- includes/Table/Table.php
+++ includes/Table/Table.php
@@ -11,20 +11,24 @@ defined( 'ABSPATH') || exit;
  */
 class Table {
 
-    public function __construct()
-    {
+    public function __construct() {
+        add_shortcode('tablescript', [$this, 'shortcodeTablescript']);
         add_action('wp_enqueue_scripts', [$this, 'enqueueScripts']);
     }
 
+    public function shortcodeTablescript($atts, $content = '', $tag = '') {
+        wp_enqueue_script('rrze-tables');
+        $script = 'jQuery(function($) { $("table.sorttable").tablesorter(); $("table.filtertable").tablesorter({widgets: ["filter"], filter_columnAnyMatch: true, }); }); ';
+        wp_add_inline_script('rrze-tables', $script, 'after' );
+        wp_enqueue_style('rrze-elements');
+    }
+
     public static function enqueueScripts() {
-        wp_enqueue_script(
+        wp_register_script(
             'rrze-tables',
             plugins_url('assets/tablesorter/js/jquery.tablesorter.combined.min.js', plugin_basename(__FILE__)),
             ['jquery'],
             '2.31.3'
         );
-        $script = 'jQuery(function($) { $("table.sorttable").tablesorter(); $("table.filtertable").tablesorter({widgets: ["filter"], filter_columnAnyMatch: true, }); }); ';
-        wp_add_inline_script('rrze-tables', $script, 'after' );
-        wp_enqueue_style('rrze-elements');
     }
 }
