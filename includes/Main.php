f5c00dd7de5a9950b8fadfc29682f0c0e6819824
Barbara Bothe <barbara.bothe@fau.de>
2023-04-05 15:56:21 +0200

    Neu: SVG List Icons

--- includes/Main.php
+++ includes/Main.php
@@ -93,7 +93,7 @@ class Main
             'rrze-elements',
             plugins_url('assets/css/rrze-elements.css', plugin_basename($this->pluginFile)),
             [],
-            '1.24.12'
+            '1.24.13'
         );
     }
 }
