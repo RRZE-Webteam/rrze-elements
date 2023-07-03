4a0615bd41288e239fbccedcf622f106a03199c1
Barbara Bothe <barbara.bothe@fau.de>
2023-07-03 13:12:41 +0200

    Farbkontrast-Funktion verbessert
    fixes #279

--- config/config.php
+++ config/config.php
@@ -43,12 +43,20 @@ function calculateContrastColor( $color ) {
     $r = hexdec( substr( $color, 0, 2 ) );
     $g = hexdec( substr( $color, 2, 2 ) );
     $b = hexdec( substr( $color, 4, 2 ) );
-    $d = '#000000';
-    // Counting the perceptive luminance - human eye favors green color...
-    $luminance = ( 0.299 * $r + 0.587 * $g + 0.114 * $b ) / 255;
-    if ( $luminance < 0.5 ) {
-        $d = '#ffffff';
-    }
+    $r2 = hexdec( 'ff' );
+    $g2 = hexdec( 'ff' );
+    $b2 = hexdec( 'ff' );
 
-    return $d;
+    $L1 = 0.2126 * pow($r/255, 2.2) +
+        0.7152 * pow($g/255, 2.2) +
+        0.0722 * pow($b/255, 2.2);
+    $L2 = 0.2126 * pow($r2/255, 2.2) +
+        0.7152 * pow($g2/255, 2.2) +
+        0.0722 * pow($b2/255, 2.2);
+    if ($L1 > $L2){
+        $d = ($L1+0.05) / ($L2+0.05);
+    } else {
+        $d = ($L2+0.05) / ($L1+0.05);
+    }
+    return ($d > 4.5 ? '#ffffff' : '#000000');
 }
